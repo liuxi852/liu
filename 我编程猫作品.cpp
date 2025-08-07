6ebe5ea080a8553dad8cd37aefcf3604ef2c9203
liuxi852 <junyu0229@163.com>
2025-08-07 14:42:49 +0800

    Add files via upload

---
+++ 我编程猫作品.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << R"## 我的编程猫作品精选
+- [飞翔的蓝雀](https://shequ.codemao.cn/my) 🐦
+- [坑爹的大冒险2.6（开源）](https://shequ.codemao.cn/my) 🎮
+- [源码音乐播放器-2](https://shequ.codemao.cn/my) 🎵
+- [ST OS 1.4.0（开源）](https://shequ.codemao.cn/my) 💻
+";
+}
